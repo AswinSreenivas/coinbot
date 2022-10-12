3ca9966049e76bfe73960125fe210310aab4cdb4
oraclown <owen.burton13@gmail.com>
2022-10-12 12:06:44 -0500

    Fix authentication

--- scrape.py
+++ scrape.py
@@ -5,23 +5,37 @@ Filter invalid mentions, send test tokens to valid mentions"""
 import tweepy
 import time
 import os
+from dotenv import load_dotenv
+import sys
 
-auth = tweepy.OAuth1UserHandler(
-    consumer_key = os.getenv('TWITTER_CONSUMER_KEY'),
-    consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET'),
-    access_token = os.getenv('TWITTER_ACCESS_TOKEN'),
-    access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+load_dotenv()
+
+auth = tweepy.OAuthHandler(
+    consumer_key = os.getenv('TWITTER_API_KEY'),
+    consumer_secret = os.getenv('TWITTER_API_SECRET'),
+)
+auth.set_access_token(
+    os.getenv('TWITTER_ACCESS_TOKEN'),
+    os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 )
 
 api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except Exception as e:
+    print(f"Error during authentication: {e}")
+    sys.exit(1)
 
 faucet_user = api.get_user('trbfaucet')
 
 yesterday_timestamp = int(time.time()) - 86400
 # get mentions since yesterday
-mentions = api.mentions_timeline(since_id=yesterday_timestamp)
+mentions = faucet_user.mentions_timeline(since_id=yesterday_timestamp)
 
 # filter out invalid mentions
 valid_mentions = []
 for mention in mentions:
-    pass
+    print("Mention from @{}: {}".format(mention.user.screen_name, mention.text))
+    print("__________________")
