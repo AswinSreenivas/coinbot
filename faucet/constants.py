e5a9aae6cbe2839025c391482778f4d9e13316f7
oraclown <owen.burton13@gmail.com>
2022-10-28 09:21:51 -0500

    Include goerli in supported

--- faucet/constants.py
+++ faucet/constants.py
@@ -1,5 +1,6 @@
 
 # mapping of chain id to network names
 SUPPORTED_CHAINS = {
-    80001: {"mumbai", "polygon testnet"}
+    80001: {"mumbai", "polygon testnet"},
+    5: {"goerli", "goerli testnet", "eth testnet"},
 }
