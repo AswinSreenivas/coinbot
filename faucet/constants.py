698c39872c1348d53dfcd4ccde8b9a7be5badb00
oraclown <owen.burton13@gmail.com>
2022-11-10 07:27:35 -0600

    Get native token symbol, structure check available funds

--- faucet/constants.py
+++ faucet/constants.py
@@ -1,6 +1,14 @@
 
-# mapping of chain id to network names
 SUPPORTED_CHAINS = {
-    80001: {"mumbai", "polygon testnet"},
-    5: {"goerli", "goerli testnet", "eth testnet"},
+    80001: {
+        "network_names": {"mumbai", "polygon testnet"},
+        "native_token_symbol": "MATIC",
+        },
+    5: {
+        "network_names": {"goerli", "goerli testnet", "eth testnet"},
+        "native_token_symbol": "ETH",
+        },
 }
+
+# Chains that do not use Playground faucet to distribute TRB
+NO_FAUCET_TRB = {5}
--- faucet/utils.py
+++ faucet/utils.py
@@ -1,11 +1,13 @@
 """
 Retrieve gas price source based on chain id
 """
-from typing import Optional
+from typing import Callable, Optional
 import requests
 from telliot_core.gas.legacy_gas import ethgasstation
 
 from logging import Logger
+
+from faucet.constants import SUPPORTED_CHAINS
 
 
 logger = Logger(__name__)
@@ -45,3 +47,38 @@ async def fetch_gas_price(chain_id: int) -> Optional[int]:
         return await fetch_goerli_gas_price()
     else:
         raise ValueError(f"Unsupported chain id: {chain_id}")
+
+
+def get_native_token_symbol(chain_id: int) -> str:
+    """Get native token symbol for given chain id."""
+    if chain_id in SUPPORTED_CHAINS:
+        return SUPPORTED_CHAINS[chain_id]["native_token_symbol"]
+    else:
+        raise ValueError(f"Unsupported chain id: {chain_id}")
+
+
+async def check_available_funds(account: str, chain_id: int, log: Callable, alert: Callable) -> bool:
+    """
+    Check if funding acount has enough testnet Goerli TRB, ETH (Goerli), & testnet MATIC.
+
+    Returns True if account has enough funds to cover gas price times
+    the estimated gas for a transfer transaction. Otherwise, returns False.
+    """
+    enough_native_token = check_native_token_funds(account=account, chain_id=chain_id)
+
+    if not enough_native_token:
+        symbol = get_native_token_symbol(chain_id=chain_id)
+        msg = f"Funding account {account} has insufficient {symbol}"
+        log(msg)
+        alert(msg)
+        return False
+    
+    if chain_id in NO_FAUCET_TRB:
+        enough_trb = check_trb_funds(account=account, chain_id=chain_id)
+        if not enough_trb:
+            msg = f"Funding account {account} has insufficient TRB"
+            log(msg)
+            alert(msg)
+            return False
+    
+    return True
